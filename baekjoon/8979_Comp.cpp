8bfc053ab0c8fe7d25b79f4d6b94cd9ebe8c2d26
inhoekim <90288195+inhoekim@users.noreply.github.com>
2021-09-25 13:23:57 +0900

    백준 - 올림픽 8979번(정렬 Ver.)

---
+++ baekjoon/8979_Comp.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int K;
+
+bool compare(vector<int> &a, vector<int> &b) {
+    if(a[0] > b[0]) return true;
+    else if(a[0] == b[0]){
+        if(a[1] > b[1]) return true;
+        else if(a[1] == b[1]) {
+            if(a[2] > b[2]) return true;
+            else if(a[2] == b[2]) {
+                if(a[3] == K) return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int N;
+    cin >> N >> K;
+    K--;
+    vector<vector<int>> list(N);
+    for(int i = 0; i < N; i++) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        a--;
+        list[a] = {b, c, d, a};
+    }
+    sort(list.begin(), list.end(), compare);
+    int ranking = 1;
+    for(vector<int> l : list) {
+        if (l[3] == K) break;
+        ranking++;
+    }
+    cout << ranking << endl;
+}
