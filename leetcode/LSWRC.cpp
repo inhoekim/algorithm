f62bdd7f9dc8cd9efe35eb0ac0b04243d1853268
inhoekim <90288195+inhoekim@users.noreply.github.com>
2021-10-02 21:26:02 +0900

    longest-substring-without-repeating-characters/

---
+++ leetcode/LSWRC.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    int static lengthOfLongestSubstring(string s) {
+        int result = 0;
+        int p = 0;
+        queue<char> q;
+        map<char,int> m;
+        char ch;
+        while(p < s.size()) {
+            if(m.find(s[p]) != m.end() && m[s[p]] != 0) {
+                do{
+                    ch = q.front();
+                    q.pop();
+                    m[ch]--;
+                }while(ch != s[p]);
+            }
+            m[s[p]] = 1;
+            q.push(s[p]);
+            int temp = q.size();
+            result = max(result, temp);
+            p++;
+        }
+        return result;
+    }
+};
+
+int main() {
+    int a = Solution::lengthOfLongestSubstring("pwwkew");
+    cout << a << endl;
+}
