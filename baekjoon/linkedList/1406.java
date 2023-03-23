5b36122682c3aff595c39c666acc7bd9621c7d1f
김인회 <90288195+inhoekim@users.noreply.github.com>
2023-03-24 00:00:46 +0900

    백준 - 1406번 에디터

--- baekjoon/linkedList/1406.java
+++ baekjoon/linkedList/1406.java
@@ -1,36 +1,37 @@
 import java.util.*;
+import java.io.*;
 
-Class Main{
-	public static void main(void){
-		Stack<Integer> stack = new Stack<>();
-		Queue<Integer> queue = new LinkedList<>();
-		BufferedReader br = new BufferedReader(new InputStream(System.in));
-		String init_str = br.readLine();
-		int m = Interger.parseInt(br.readLine());
-		
-		for (char ch : init_str.toCharArray())
-			stack.add(ch);
-		for (int i = 0; i < m; i++){
-			String oper = br.readLine();
-			if (oper.charAt(0) == 'P')
-				stack.add(oper.charAt(2));
-			else if (oper.charAt(0) == 'L')
-				if (!stack.empty())
-					queue.add(stack.pop());
-			else if (oper.charAt(0) == 'D')
-				if (!queue.isEmpty())
-					stack.add(queue.poll());
-			else{
-				if (!stack.empty)
-					stack.pop();
-			}
-		}
-		Stakc<Integer> print_stack = new Stack<>();
-		while (!stack.empty())
-			print_stack.add(stack.pop());
-		while (!print_stack.empty())
-			System.out.print(print_stack.pop());
-		while (!queue.isEmpty())
-			System.out.print(queue.poll());
-	}
+class Anything{
+    public static void main(String[] argv) throws IOException {
+        Stack<Character> stack_left = new Stack<>();
+        Stack<Character> stack_right = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String init_str = br.readLine();
+        int m = Integer.parseInt(br.readLine());
+
+        for (char ch : init_str.toCharArray())
+            stack_left.add(ch);
+        for (int i = 0; i < m; i++) {
+            String oper = br.readLine();
+            if (oper.charAt(0) == 'P')
+                stack_left.add(oper.charAt(2));
+            else if (oper.charAt(0) == 'L') {
+                if (!stack_left.empty())
+                    stack_right.add(stack_left.pop());
+            } else if (oper.charAt(0) == 'D') {
+                if (!stack_right.isEmpty())
+                    stack_left.add(stack_right.pop());
+            } else {
+                if (!stack_left.empty())
+                    stack_left.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack_right.isEmpty())
+            stack_left.add(stack_right.pop());
+        while (!stack_left.empty())
+            sb.append(stack_left.pop());
+        sb.reverse();
+        System.out.println(sb);
+    }
 }
