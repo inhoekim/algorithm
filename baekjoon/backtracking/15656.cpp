d85f135e5ec8ec37900933ac2df11a4220649635
김인회 <90288195+inhoekim@users.noreply.github.com>
2023-06-18 18:41:16 +0900

    백준 - 15656번 (N과 M [7])

---
+++ baekjoon/backtracking/15656.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> picked;
+stringstream ss;
+
+void pick_num(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			if (i == m - 1)
+				ss << picked[i] << endl;
+			else
+				ss << picked[i] << " ";
+		}
+		return ;
+	}
+	for (int i = 0; i < n; i++) {
+		picked.push_back(nums[i]);
+		pick_num(cnt + 1);
+		picked.pop_back();
+	}
+}
+int main(void) {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		nums.push_back(temp);
+	}
+	sort(nums.begin(), nums.end());
+	pick_num(0);
+	cout << ss.str();
+}
